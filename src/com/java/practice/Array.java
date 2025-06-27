dbab528df64df6fe34182ca1d5ed04c188b1dd16
prnprasadapepdcl <prnprasadapepdcl@gmail.com>
2025-06-27 23:25:58 +0530

    cmt3

---
+++ src/com/java/practice/Array.java
@@ -0,0 +1,12 @@
+package com.java.practice;
+
+public class Array {
+	public static void main(String[] args) {
+		String a[]={"prn","prasad"};
+		for(String e:a)
+		{
+			System.out.println(e);
+		}
+	}
+
+}
