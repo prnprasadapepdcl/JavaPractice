70aabeeb5a13bb6a37c000f5784731d1b8a2065f
prnprasadapepdcl <prnprasadapepdcl@gmail.com>
2025-06-27 23:40:07 +0530

    dev cm2

---
+++ src/com/java/practice/ArrayListdDemo.java
@@ -0,0 +1,14 @@
+package com.java.practice;
+
+import java.util.ArrayList;
+
+public class ArrayListdDemo {
+	public static void main(String[] args) {
+	ArrayList<Integer> a = new ArrayList<Integer>();
+		a.add(3);
+		a.add(4);
+		
+		for(int  i:a) {
+			System.out.println(i);		}
+	}
+}
