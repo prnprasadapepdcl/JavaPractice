4edc1594055220fd999850bd2de5d1959265c036
prnprasadapepdcl <prnprasadapepdcl@gmail.com>
2025-06-27 23:28:15 +0530

    dev cmt1

--- src/com/java/practice/Array.java
+++ src/com/java/practice/Array.java
@@ -2,7 +2,7 @@ package com.java.practice;
 
 public class Array {
 	public static void main(String[] args) {
-		String a[]={"prn","prasad"};
+		String a[]={"prn","prasad","raja"};
 		for(String e:a)
 		{
 			System.out.println(e);
